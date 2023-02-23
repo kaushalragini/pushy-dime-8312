18fb6ca835421b96389e5830bb72d1bd06da556c
devarshi009 <devarshiojha009@gmail.com>
2023-02-23 23:41:18 +0530

    login signup page created

--- frontend/fashionstreet/src/Components/devarshi/Signup/Signup.jsx
+++ frontend/fashionstreet/src/Components/devarshi/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import {
-    Box,
+  Box,
   Button,
   Checkbox,
   Flex,
@@ -20,76 +20,70 @@ import {
   ModalOverlay,
   Stack,
   Text,
-  
-  useColorModeValue,
-  
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
- import { MdOutlineDataSaverOn } from "react-icons/md";
- import { FaHeart } from "react-icons/fa";
- import { AiFillTrophy } from "react-icons/ai";
-import bag from "./png/bag.png";
-import bell from "./png/bell.png";
-import google from "./png/google.jpg";
-import facebook from "./png/Facebook_icon.png";
-import apple from "./png/Apple_logo_black.jpg";
-import wechat from "./png/wechat.png";
+import Login from "../Login/Login";
+import React, { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom"
+import { MdOutlineDataSaverOn } from "react-icons/md";
+import { FaHeart } from "react-icons/fa";
+import { AiFillTrophy } from "react-icons/ai";
+import bag from "../png/bag.png";
+import bell from "../png/bell.png";
+import google from "../png/google.jpg";
+import facebook from "../png/Facebook_icon.png";
+import apple from "../png/Apple_logo_black.jpg";
+import wechat from "../png/wechat.png";
 // import note from "./png/note.jpg";
 
-const ModalCompo = ({name}) => {
-  const OverlayOne = () => (
-    <ModalOverlay
-      bg="blackAlpha.300"
-      backdropFilter="blur(10px) hue-rotate(90deg)"
-    />
-  );
+const Signup = () => {
+  const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+   
+  
 
-  const OverlayTwo = () => (
-    <ModalOverlay
-      bg="none"
-      backdropFilter="auto"
-      backdropInvert="80%"
-      backdropBlur="2px"
-    />
-  );
+  
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [overlay, setOverlay] = React.useState(<OverlayOne />);
+  
+ 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     onOpen();
+  //   }, 5000);
+  // }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      onOpen();
-    }, 5000);
-  }, []);
+  const handleSubmit=()=>{
+    const payload={
+        
+        email,
+        password
+    }
+    fetch("http://localhost:8080/users/register",{
+        method:"POST",
+        body:JSON.stringify(payload),
+        headers:{
+            "Content-type":"application/json"
+        },
+       
+    }).then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.log(err))
+}
 
+// const sendLogin=()=>{
+// navigate()
+// }
   return (
-    <>
-      {/* <Button
-        onClick={() => {
-          setOverlay(<OverlayOne />);
-          onOpen();
-        }}
-      >
-        Modal 1
-      </Button> */}
-      <Button
-        ml="4"
-        onClick={() => {
-          setOverlay(<OverlayTwo />);
-          onOpen();
-        }}
-      >
-        {name}
-      </Button>
-      <Modal isCentered isOpen={isOpen} onClose={onClose} size="full">
-        {overlay}
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Grid templateColumns="repeat(2, 1fr)" ml={200} mr={150} mt={"150px"}>
-              <GridItem w="100%" h="800px" bg="black">
+   
+      
+     
+       <div>
+      
+
+      {/* -------------I AM GOING TO COMMENT THIS FOR SOME TIME======== */}
+      <Grid templateColumns="repeat(2, 1fr)" ml={200} mr={150} mt={"150px"}>
+        {/* ================I HAVE TO PUT THE LINE HERE+++++++++++++++ */}
+        <GridItem w="100%" h="800px" bg="black">
                 <Heading
                   color={"white"}
                   fontSize={75}
@@ -158,10 +152,10 @@ const ModalCompo = ({name}) => {
                   // textAlign={"left"}
                 >
                   <Image src={bag} alt="bag" w={"30px"} h={"30px"} />
-                  <span
+                  <Text
                     style={{
                       position: "relative",
-                      left: "40px",
+                      right: "80px",
                       bottom: "23px",
                       fontWeight: "400",
                       fontSize: "18px",
@@ -169,19 +163,19 @@ const ModalCompo = ({name}) => {
                     }}
                   >
                     COMPARE ACROSS 500+ STORES
-                  </span>
+                  </Text>
                 </Heading>
 
                 <Heading
                   color={"white"}
                   fontSize={"16px"}
-                  // letterSpacing={2}
+                 
                   fontWeight={400}
                   mt={"5px"}
                   ml={20}
-                  // textAlign={"left"}
+                 
                 >
-                  {/* <BsBell color={"white"} fontSize={"25px"}/> */}
+                 
                   <Image
                     src={bell}
                     alt="bell"
@@ -189,10 +183,10 @@ const ModalCompo = ({name}) => {
                     h={"30px"}
                     bg={"none"}
                   />
-                  <span
+                   <Text 
                     style={{
                       position: "relative",
-                      left: "40px",
+                      right: "90px",
                       bottom: "27px",
                       fontWeight: "400",
                       fontSize: "18px",
@@ -200,7 +194,7 @@ const ModalCompo = ({name}) => {
                     }}
                   >
                     GET ALERTS ON YOUR ITEMS
-                  </span>
+                  </Text>
                 </Heading>
 
                 <Heading
@@ -213,10 +207,10 @@ const ModalCompo = ({name}) => {
                 >
                   {/* <Image src={note} alt="bag" w={"25px"} h={"20px"} color={"white"}/> */}
                   <MdOutlineDataSaverOn fontSize={"30px"} />
-                  <span
+                  <Text
                     style={{
                       position: "relative",
-                      left: "40px",
+                      right: "100px",
                       bottom: "27px",
                       fontWeight: "400",
                       fontSize: "18px",
@@ -224,7 +218,7 @@ const ModalCompo = ({name}) => {
                     }}
                   >
                     SAVE YOUR SEARCHES
-                  </span>
+                  </Text>
                 </Heading>
 
                 <Heading
@@ -238,17 +232,17 @@ const ModalCompo = ({name}) => {
                 >
                     <FaHeart fontSize={"30px"}/>
                   {/* <Image src={bag} alt="bag" w={"25px"} h={"20px"} /> */}
-                  <span
+                  <Text
                     style={{
-                      position: "relative",
-                      left: "40px",
+                      position:"relative",
+                      right: "50px",
                       bottom: "25px",
                       fontWeight: "400",
                       fontSize: "18px",
                     }}
                   >
                     MANAGE YOUR SHOPPING LISTS
-                  </span>
+                  </Text>
                 </Heading>
 
                 <Heading
@@ -262,28 +256,29 @@ const ModalCompo = ({name}) => {
                 >
                     <AiFillTrophy fontSize={"30px"}/>
                   {/* <Image src={bag} alt="bag" w={"25px"} h={"20px"} /> */}
-                  <span
+                  <Text
                     style={{
                       position: "relative",
-                      left: "40px",
+                      right: "145px",
                       bottom: "27px",
                       fontWeight: "400",
                       fontSize: "18px",
                     }}
                   >
                     EARN POINTS
-                  </span>
+                  </Text>
                 </Heading>
               </GridItem>
-              {/* =========================Signup Card======================================== */}
-              <GridItem w="100%" h="800px">
-             
 
-    <Flex
+        {/* =========================Signup Card======================================== */}
+        <GridItem w="100%" h="800px">
+         
+
+          <Flex
       minH={'100vh'}
      align={'center'}
       justify={'center'}
-    //   bg={useColorModeValue('gray.50', 'gray.800')}
+    
       >
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
@@ -295,63 +290,87 @@ const ModalCompo = ({name}) => {
             Check UpStyle befire you buy.
           </Text>
         </Stack>
-        <Box
-          rounded={'lg'}
-        //   bg={useColorModeValue('white', 'gray.700')}
-        //   boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl>
-             
-              <Input type="email" paddingRight={"200px"} paddingLeft={"30px"} placeholder="Email"/>
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Subscribe to personalized sale updates and offers</Checkbox>
-                {/* <Link color={'blue.400'}>Forgot password?</Link> */}
-              </Stack>
-              <Button
-                bg={'black'}
-                color={'white'}
-                _hover={{
-                  bg: 'black',
-                }}>
-                SIGN UP
-              </Button>
-            </Stack>
-          </Stack>
-          <Heading fontSize={"15px"} color={"gray.500"} ml="200px" mt="40px">or</Heading>
+          <Box rounded={"lg"} p={8}>
+            <Box>
+              
+                <Stack spacing={4}>
+                  <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                      type="email"
+                      name="email"
+                      value={email}
+                      onChange={(e)=>setEmail(e.target.value)}
+                      paddingRight={"200px"}
+                      paddingLeft={"30px"}
+                      placeholder="Email"
+                    />
+                  </FormControl>
+                  {/* <Text>{formErrors.email}</Text> */}
+                  <FormControl id="password">
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e)=>setPassword(e.target.value)}
+                    />
+                  </FormControl>
+                  {/* <Text>{formErrors.password}</Text> */}
+                  {/* <Input type="submit" /> */}
 
-          <Box display="flex" gap="70px" mt="20px">
-             <Image src={google} w="10%" objectFit={"contain"}/>
-             <Image src={facebook}w="8%" objectFit={"contain"}  />
-             <Image src={apple} w="8%" objectFit={"contain"}  />
-             <Image src={wechat} w="10%" objectFit={"contain"} />
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: "column", sm: "row" }}
+                      align={"start"}
+                      justify={"space-between"}
+                    >
+                      <Checkbox>
+                        Subscribe to personalized sale updates and offers
+                      </Checkbox>
+                      {/* <Link color={'blue.400'}>Forgot password?</Link> */}
+                    </Stack>
+
+                    <Button
+                      bg={"black"}
+                      color={"white"}
+                      _hover={{
+                        bg: "black",
+                      }}
+                     onClick={handleSubmit}>
+                      SIGN UP
+                    </Button>
+                  </Stack>
+                </Stack>
+            
+            </Box>
+            <Heading fontSize={"15px"} color={"gray.500"}  mt="40px">
+              or
+            </Heading>
+
+            <Box display="flex" gap="70px" mt="20px">
+              <Image src={google} w="10%" objectFit={"contain"} />
+              <Image src={facebook} w="8%" objectFit={"contain"} />
+              <Image src={apple} w="8%" objectFit={"contain"} />
+              <Image src={wechat} w="10%" objectFit={"contain"} />
+            </Box>
+            <Text mt="20px" textAlign={"center"} >
+              Already have an account? Please sign in
+            </Text>
           </Box>
-          <Text mt="20px" textAlign={"center"}>Already have an account? Please sign in</Text>
-        </Box>
-      </Stack>
+          </Stack>
    
-    </Flex>
-  
+    </Flex> 
+        </GridItem>
+        </Grid>  
 
-              </GridItem>
-            </Grid>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+      
+        
+      
+   
+     </div>
   );
 };
 
-export default ModalCompo;
+export default Signup;
