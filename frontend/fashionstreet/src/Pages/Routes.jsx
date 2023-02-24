2ad7d14e926f7e0d1d5dccdf61d0280c55cc2664
princepatel7405 <prince.m.7405@gmail.com>
2023-02-24 22:27:20 +0530

    day-4 work done

--- frontend/fashionstreet/src/Pages/Routes.jsx
+++ frontend/fashionstreet/src/Pages/Routes.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import Products from "../Components/nikhil/Products";
-import SingleProduct from "../Components/nikhil/SingleProduct";
+import { Route, Routes } from "react-router-dom";
+import AdminHomePage from "../Admin/Pages/AdminHomePage";
+//import AdminProducts from "../Admin/Pages/AdminProducts";
+import Home from "./Home";
+// import Products from "../Components/nikhil/Products";
+// import SingleProduct from "../Components/nikhil/SingleProduct";
 
-const Routes = () => {
+const Routing = () => {
   return (
     <Routes>
-      <Route path="/" element={"home"} />
+      <Route path="/" element={<Home />} />
       <Route path="/men-fashion" element={"menfashion"} />
       <Route path="/women-fashion" element={"womenfashion"} />
       <Route path="/shop-beauty" element={"shopbeauty"} />
       <Route path="/kid-fashion" />
       <Route path="/" />
+      <Route path="/admin" element={<AdminHomePage/>}/>
+      {/* <Route path="/admin/products" element={<AdminProducts />} /> */}
 
-      <Route path="/products" element={<Products />} />
-      <Route path="/products/:id" element={<SingleProduct />} />
+      {/* <Route path="/products" element={<Products />} /> */}
+      {/* <Route path="/products/:id" element={<SingleProduct />} /> */}
     </Routes>
   );
 };
 
-export default Routes;
+export default Routing;
